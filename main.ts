178fdb4bed1b29fdf7e29e3442765a548ecf0ee8
Fatouseck06 <125355360+Fatouseck06@users.noreply.github.com>
2023-03-23 09:48:15 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,10 @@
 input.onButtonPressed(Button.A, function () {
-    radio.sendString(" Hola como estas?")
-})
-input.onSound(DetectedSound.Loud, function () {
-    radio.sendString("Estoy bien ")
+    radio.sendString("fatou")
+    basic.showIcon(IconNames.Yes)
+    basic.pause(200)
+    basic.clearScreen()
 })
 radio.onReceivedString(function (receivedString) {
     basic.showString(receivedString)
 })
-input.onGesture(Gesture.Shake, function () {
-    radio.sendString("Hola ")
-})
 radio.setGroup(1)
-input.setSoundThreshold(SoundThreshold.Loud, 192)
